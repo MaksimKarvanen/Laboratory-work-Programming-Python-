6337e7e584e462037159531dc16dd0601b736234
MaksimKarvanen <maksimkarvanen03@gmail.com>
2025-09-18 19:59:57 +0300

    lab5 ex 3-5

--- lab5/lab5_python.py
+++ lab5/lab5_python.py
@@ -12,6 +12,61 @@ def ex2(data):
         symbols = sum(len(line) for line in lines)
         print(f"Строк: {length}, Слов: {words}, Символов: {symbols}")
 
+def ex3(data, newfile):
+    with open(data, 'r') as data_file:
+        text = data_file.read()
+    with open(newfile, 'w') as new_file:
+        new_file.write(text)
+    print("Копирование завершено!")
+
+def ex4(data):
+    with open(data, 'r') as file:
+        lines = file.readlines()
+        for i, line in enumerate(lines, start=1):
+            print(f"{i}: {line.strip()}")
+        
+def ex5(data):
+    with open(data, 'a') as file:
+        file.write("\nЭто новая седьмая строка, добавленная в задании 5.")
+
+def ex6(data):
+    vowels = 'аеиоуыэюяaeiouy'
+    with open(data, 'r') as file:
+        text = file.read().lower()
+        count = sum(1 for char in text if char in vowels)
+        print(f"Количество гласных: {count}")
+
+def ex7():
+    pass
+
+def ex8():
+    pass
+
+def ex9():
+    pass
+
+def ex10():
+    pass
+
+def ex11():
+    pass
+
+def ex12():
+    pass
+
+def ex13():
+    pass
+
+def ex14():
+    pass
+
+def ex15():
+    pass
+
 
 ex1("data.txt")
 ex2("data.txt")
+ex3("data.txt", "newfile.txt")
+ex4("data.txt")
+ex5("data.txt")
+ex6("data.txt")
