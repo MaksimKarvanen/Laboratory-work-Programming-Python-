f44da2df00e6127bc957d376da1e03e794fdc9db
MaksimKarvanen <maksimkarvanen03@gmail.com>
2025-09-19 11:57:45 +0300

    lab5 ex 1-10

--- lab5/lab5_python.py
+++ lab5/lab5_python.py
@@ -1,3 +1,7 @@
+import csv
+import random
+
+
 def ex1(data):
     with open(data, 'r') as file:
         lines = file.readlines()
@@ -27,7 +31,7 @@ def ex4(data):
         
 def ex5(data):
     with open(data, 'a') as file:
-        file.write("\nЭто новая седьмая строка, добавленная в задании 5.")
+        file.write("\nЭто новая строка, добавленная в задании 5.")
 
 def ex6(data):
     vowels = 'аеиоуыэюяaeiouy'
@@ -36,17 +40,32 @@ def ex6(data):
         count = sum(1 for char in text if char in vowels)
         print(f"Количество гласных: {count}")
 
-def ex7():
-    pass
+def ex7(data):
+    with open(data, 'r') as file:
+        for line in file:
+            for word in line.split():
+                print(word[0].upper() + word[1:], end=' ')
 
-def ex8():
-    pass
+def ex8(newdata):
+    with open(newdata, 'w') as file:
+        for i in range(10):
+            random_list = [random.randint(1, 100) for _ in range(10)]
+            file.write(' '.join(map(str, random_list)) + '\n')
+    print(f'Список с рандомными числами записан в {newdata}')
 
-def ex9():
-    pass
+def ex9(data):
+    with open(data, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            print(f"ID: {row['id']}, Name: {row['name']}, Department: {row['department']}, Salary: {row['salary']}, Start Date: {row['start_date']}, Active: {row['active']}")
 
-def ex10():
-    pass
+def ex10(data):
+    try:
+        with open(data, 'r') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print("Файл не найден. Пожалуйста, проверьте имя файла и попробуйте снова.")
 
 def ex11():
     pass
@@ -70,3 +89,7 @@ ex3("data.txt", "newfile.txt")
 ex4("data.txt")
 ex5("data.txt")
 ex6("data.txt")
+ex7("data.txt")
+ex8("random_numbers.txt")
+ex9("data.csv")
+ex10("non_existent_file.txt")
